0279a86a55d4b35cd764f71a4dc50c25dba6d7e8
alexdand1 <adiaz@solstice.com>
2020-04-08 10:25:47 -0300

    Adds addpoll component

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,32 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { handleInitialDataLoad } from '../actions/shared';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { handleInitialDataLoad } from "../actions/shared";
 
-import DashBoard from './DashBoard';
-import LoadingBar from 'react-redux-loading';
-import Leaderboard from './Leaderboard';
+import DashBoard from "./DashBoard";
+import AddPoll from "./AddPoll";
+import LoadingBar from "react-redux-loading";
+import Leaderboard from "./Leaderboard";
 
 class App extends Component {
-	componentDidMount() {
-		const { dispatch } = this.props;
-		dispatch(handleInitialDataLoad());
-	}
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(handleInitialDataLoad());
+  }
 
-	render() {
-		return (
-			<div>
-				<LoadingBar />
-				{/* {this.props.loading ? null : <DashBoard />} */}
-				{this.props.loading ? null : <Leaderboard />}
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <LoadingBar />
+        {/* {this.props.loading ? null : <DashBoard />} */}
+        {/* {this.props.loading ? null : <Leaderboard />} */}
+        {this.props.loading ? null : <AddPoll />}
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = ({ authedUser }) => ({
-	loading: authedUser === null
+  loading: authedUser === null,
 });
 
 export default connect(mapStateToProps)(App);
